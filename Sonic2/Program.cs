c0f3e486ab2d447cc85747a8351df1f7202bc6fb
megabspam <megabspam@yandex.ru>
2021-04-24 20:54:03 +0700

    release

--- Sonic2/Program.cs
+++ Sonic2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace Sonic2
     {
         static void Main(string[] args)
         {
+            StreamReader fs1 = new StreamReader(args[0]);
+            StreamWriter result = new StreamWriter(args[0] + ".name");
+
+            string s = "";
+            while (s != null)
+            {
+                s = fs1.ReadLine();
+                if (s != null)
+                {
+                    var ss = s.Split(',');
+                    if (ss.Count() > 4)
+                        if ((ss[2].Length > 3)&((ss[3].Length > 3)))
+                            result.WriteLine(ss[2].Substring(1, ss[2].Length - 2)+" "+ ss[3].Substring(1, ss[3].Length - 2));
+                }
+            }
+            result.Close();
         }
     }
 }
